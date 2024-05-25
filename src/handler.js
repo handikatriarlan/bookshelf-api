dc6f72daacb2d037bd392d045ea695f8284526cc
Arlan Tri Handika <handikaarlan@gmail.com>
2024-05-25 14:15:23 +0700

    Added addBookHandler

--- src/handler.js
+++ src/handler.js
@@ -1,33 +1,69 @@
 const { nanoid } = require('nanoid')
 const books = require('./books')
-const addbookHandler = (request, h) => {
-  const { title, tags, body } = request.payload
-  const id = nanoid(16)
-  const createdAt = new Date().toISOString()
-  const updatedAt = createdAt
-  const newbook = {
-    title, tags, body, id, createdAt, updatedAt
+const addBookHandler = (request, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    });
+    response.code(400);
+    return response;
   }
-  books.push(newbook)
-  const isSuccess = books.filter((book) => book.id === id).length > 0
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    });
+    response.code(400);
+    return response;
+  }
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
   if (isSuccess) {
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil ditambahkan',
+      message: 'Buku berhasil ditambahkan',
       data: {
         bookId: id
       }
-    })
-    response.code(201)
-    return response
+    });
+    response.code(201);
+    return response;
   }
+
   const response = h.response({
     status: 'fail',
-    message: 'Catatan gagal ditambahkan'
-  })
-  response.code(500)
-  return response
-}
+    message: 'Buku gagal ditambahkan'
+  });
+  response.code(500);
+  return response;
+};
 
 const getAllbooksHandler = () => ({
   status: 'success',
